ca9a121440614fbf76d71b8914386270fd1dd9de
KFHA <kfha@kb.dk>
2025-03-14 13:01:03 +0100

    hvis der er tekstkolonner et sted hvor de ikke burde være

--- Kommaformatering/find_konverter.py
+++ Kommaformatering/find_konverter.py
@@ -16,14 +16,27 @@ if __name__ == "__main__":
     from tqdm import tqdm
     from analyse import lav_overblik
     # Kører alle filerne igennem for at tjekke, at de kan konverteres. Anbefales at gøre før push.
-    mappe = "CSV"
+
+    MAX_FILE_SIZE = 40 * 1024 * 1024  # hvis du er doven og ikke vil vente længe...
+    
+
+    mappe = "TSV"
     base_sti = os.path.join("F:", "BP", "ALF", "ALF organisation", "Grupper", "Analysegruppen", "Kommaformatering", "Filer med dårligt format", mappe)
     filer = os.listdir(base_sti)
     filer = [fil for fil in filer if "." in fil]
     print("Konverterer filer fra mappen:", mappe)
     tqdm_bar = tqdm(filer, desc="Konverterer filer")
     for fil in tqdm_bar:
-        tqdm_bar.set_postfix(fil=fil)
         sti = os.path.join(base_sti, fil)
+
+        fil_sti = os.path.join(base_sti, fil)
+        file_size = os.path.getsize(fil_sti)
+        
+        if file_size > MAX_FILE_SIZE:
+            print(f"Springer {fil} over - filen er for stor ({file_size / (1024*1024):.1f} MB)")
+            continue
+
+        tqdm_bar.set_postfix(fil=fil)
+
         output_file, df_cleaned = find_konverter(sti)
         forlag_brug = lav_overblik(df_cleaned)
